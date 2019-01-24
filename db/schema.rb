a7c73f100803ea643e86ce1de6cc864117e72dc4
Edgar Jr San Martin <edgar.sanmartinjr@gmail.com>
2019-01-24 13:46:35 -0500

    Comment functionality completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_22_090507) do
+ActiveRecord::Schema.define(version: 2019_01_24_085500) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
